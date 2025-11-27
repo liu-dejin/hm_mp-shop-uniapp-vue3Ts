5fad5c08c5db3ee645481ab8ca7d094a4da95944
liu-dejin <liudejin@kis2.cn>
2025-11-27 22:07:07 +0800

    feat: 商品详情弹出层基本使用

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -37,6 +37,12 @@ const onTapImage = (url: string) => {
     current: url,
   })
 }
+
+// uni-ui弹出层ref
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
 </script>
 
 <template>
@@ -77,7 +83,7 @@ const onTapImage = (url: string) => {
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="popup?.open('bottom')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -147,6 +153,11 @@ const onTapImage = (url: string) => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <view>内容1</view>
+    <view>内容1</view>
+    <button>关闭</button> </uni-popup
+  >>
 </template>
 
 <style lang="scss">
